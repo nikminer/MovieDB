84d0fac6896eb162cd05e1a254bab46c0e19479c
nikminer <nikminerfeed@gmail.com>
2019-11-01 00:31:59 +0300

    v 0.2.7.1

--- SerialDB/Main/views/index.py
+++ SerialDB/Main/views/index.py
@@ -3,14 +3,14 @@ from Profile.models import Profile,Friendlist
 from django.db.models import Q
 
 def index(request):
-    profile=Profile.objects.get(user__username=request.user.username)
-    myfriends=Friendlist.objects.filter(Q(accepter=profile) | Q(sender=profile))
-    data={
-        "profile":profile,
-        "friends":{
-            "friends":myfriends.filter(status=1).count(),
-            "requests":myfriends.filter(status=0).count()
-        }
-        
-    }
+    data={}
+    if request.is_authenticated:
+        profile=Profile.objects.get(user__username=request.user.username)
+        data.update({"profile":profile})
+        myfriends=Friendlist.objects.filter(Q(accepter=profile) | Q(sender=profile))
+        data.update({"friends":{
+                "friends":myfriends.filter(status=1).count(),
+                "requests":myfriends.filter(status=0).count()  
+            }
+        })
     return render(request,"Main/index.html",data)
--- SerialDB/Profile/models.py
+++ SerialDB/Profile/models.py
@@ -2,7 +2,6 @@
 from django.db import models
 from django.conf import settings
 from datetime import datetime
-from image_cropping import ImageRatioField
 
 class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
