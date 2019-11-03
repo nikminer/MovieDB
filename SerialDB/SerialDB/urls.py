a05a911aee6dec613f156378babdb58fe78da237
nikminer <nikminerfeed@gmail.com>
2019-11-03 12:06:14 +0300

    connect robots.txt

--- SerialDB/SerialDB/urls.py
+++ SerialDB/SerialDB/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from django.conf.urls import include, url
-
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -12,12 +12,14 @@ from Films import urls as urlsFilms
 from Profile import urls as urlsProfile
 from Profile.views.auth import loginView
 
+
 urlpatterns = [
     path('',views.index.index,name="index"),
     path('add/',views.addmovie.AddMoviePage,name='addmovie'),
     path('add/kinopoisk/search/',views.addmovie.Search),
     path('add/kinopoisk/<int:id>/',views.addmovie.AddMovie),
 
+    path('robots.txt', TemplateView.as_view(template_name="robots.txt", content_type='text/plain')),
     path('list/',include(urlsList), name='list'),
     path('serial/',include(urlsSerials), name='serial'),
     path('film/',include(urlsFilms), name='film'),
