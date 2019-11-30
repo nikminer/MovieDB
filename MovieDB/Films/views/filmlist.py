44ad81d088beb3a5fe4973c7d4022637f58d959a
nikminer <nikminerfeed@gmail.com>
2019-11-30 15:48:19 +0300

    fix filmlist

--- MovieDB/Films/views/filmlist.py
+++ MovieDB/Films/views/filmlist.py
@@ -7,7 +7,7 @@ import datetime
 def FilmList(request):
     
 
-    FilmList= Film.objects.all().order_by('year') 
+    FilmList= Film.objects.all().order_by('-year') 
     for film in FilmList:
         film.InMyList=str(len(UserListF.objects.filter(film_id=film.id,user=request.user.id))>0)
         
