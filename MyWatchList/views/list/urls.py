2774111cb7e23eeac6dcdb89ad25ab471870c8a3
nikminer <nikminerfeed@gmail.com>
2020-03-25 22:34:38 +0300

    adapting to new db

--- MyWatchList/views/list/urls.py
+++ MyWatchList/views/list/urls.py
@@ -1,16 +1,10 @@
 from django.urls import path
-from List import views
 from MyWatchList.views import list
 
 urlpatterns = [
-    path('<str:username>/series', list.watchlist_series ,name='listserial'),
-
-    path('add/serial/<int:id>', views.serials.AddSerial,name='addserial'),
-    path('del/serial/<int:id>',views.serials.DelSerial,name='delserial'),
-
-
+    path('<str:username>/series', list.watchlist_series, name='listserial'),
     path('<str:username>/films', list.watchlist_films,name='listfilm'),
 
-    path('add/film/<int:id>', views.film.AddFilm,name='addfilm'),
-    path('del/film/<int:id>', views.film.DelFilm,name='delfilm'),
+    path('add/<int:movie_id>', list.addlist, name='addlist'),
+    path('del/<int:movie_id>', list.dellist, name='dellist'),
 ]
